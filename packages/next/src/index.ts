709d4984683c33df9dd132c5cf2ce093de884d87
NEDDL <mustafasaitonal@outlook.com>
2025-04-11 18:20:38 +0200

    fix: types

--- packages/next/src/index.ts
+++ packages/next/src/index.ts
@@ -1,1 +1,2 @@
-export { ConsentManagerProvider, useConsentManager } from "./ConsentManagerProvider";
+export * from "./Scripts";
+export * from "./ConsentManagerProvider";
--- packages/next/tsup.config.ts
+++ packages/next/tsup.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
-  dts: false,
+  dts: false, // To build locally
   sourcemap: true,
   clean: true,
   target: "es2020",
